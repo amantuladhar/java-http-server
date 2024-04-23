8aab7858741085f81bf5bf0a0ac51de13a269fc5
Aman Tuladhar <atuladhar.aman@gmail.com>
2024-04-23 13:30:07 -0500

    First stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,23 +2,23 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    log.info("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    // ServerSocket serverSocket = null;
-    // Socket clientSocket = null;
-    //
-    // try {
-    //   serverSocket = new ServerSocket(4221);
-    //   serverSocket.setReuseAddress(true);
-    //   clientSocket = serverSocket.accept(); // Wait for connection from client.
-    //   System.out.println("accepted new connection");
-    // } catch (IOException e) {
-    //   System.out.println("IOException: " + e.getMessage());
-    // }
+    ServerSocket serverSocket = null;
+    Socket clientSocket = null;
+
+    try {
+      serverSocket = new ServerSocket(4221);
+      serverSocket.setReuseAddress(true);
+      clientSocket = serverSocket.accept(); // Wait for connection from client.
+      log.info("accepted new connection");
+    } catch (IOException e) {
+      log.info("IOException: " + e.getMessage());
+    }
   }
 }
