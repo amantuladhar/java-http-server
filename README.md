928c29077aa6072ca59b4728e2b5fc554f984d68
Aman Tuladhar <atuladhar.aman@gmail.com>
2024-04-26 20:20:54 -0500

    Make it multi threaded

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 [![progress-banner](https://backend.codecrafters.io/progress/http-server/9aec27d7-47f5-43ea-83c4-108c9ebf54ea)](https://app.codecrafters.io/users/codecrafters-bot?r=2qF)
 
-Java implementation of a simple HTTP server.
+Java implementation of a simple HTTP server using virtual threads.
+
+## Notes
+
+- `nc localhost 4221` to keep the active connection to test your multi connection
--- src/main/java/server/HttpServer.java
+++ src/main/java/server/HttpServer.java
@@ -2,9 +2,14 @@ package server;
 
 import static lombok.AccessLevel.PRIVATE;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import http.HTTPMethod;
 import http.Request;
 import http.Response;
@@ -17,12 +22,23 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor(access = PRIVATE)
 public class HttpServer {
     private final Map<RouteMappingKey, HttpRequestProcessor> mappings;
+    private final ExecutorService executorService;
 
     public void start(int port) {
         try (var ss = new ServerSocket(port)) {
             ss.setReuseAddress(true);
-            while (true) {
-                try (var cs = ss.accept()) {
+            handleConnection(ss);
+        } catch (Exception e) {
+            log.error("Unknown error: " + e.getMessage(), e);
+        }
+    }
+
+    private void handleConnection(ServerSocket ss) throws IOException {
+        while (true) {
+            log.info("Waiting for client connection....");
+            Socket cs = ss.accept();
+            executorService.execute(() -> {
+                try {
                     var req = Request.from(cs.getInputStream(), mappings.keySet());
 
                     var key = new RouteMappingKey(req.getMethod(), req.getPattern());
@@ -34,11 +50,18 @@ public class HttpServer {
                     var os = cs.getOutputStream();
                     os.write(resp.toBytes());
                     os.flush();
+                } catch (IOException e) {
+                    log.error("Something is wrong -- ", e);
+                } finally {
+                    try {
+                        cs.close();
+                    } catch (IOException e) {
+                        log.error("Unable to close client socket.", e);
+                    }
                 }
-            }
-        } catch (Exception e) {
-            log.error("Unknown error: " + e.getMessage(), e);
+            });
         }
+
     }
 
     public static HttpServerBuilder builder() {
@@ -48,6 +71,12 @@ public class HttpServer {
     @NoArgsConstructor(access = PRIVATE)
     public static class HttpServerBuilder {
         private HashMap<RouteMappingKey, HttpRequestProcessor> mappings = new HashMap<>();
+        private ExecutorService executorService;
+
+        public HttpServerBuilder executorService(ExecutorService es) {
+            this.executorService = es;
+            return this;
+        }
 
         public HttpServerBuilder get(String location, HttpRequestProcessor processor) {
             mappings.put(new RouteMappingKey(HTTPMethod.GET, location), processor);
@@ -60,7 +89,10 @@ public class HttpServer {
         }
 
         public HttpServer build() {
-            return new HttpServer(mappings);
+            if (executorService == null) {
+                executorService = Executors.newVirtualThreadPerTaskExecutor();
+            }
+            return new HttpServer(mappings, executorService);
         }
     }
 
